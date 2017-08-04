929453c0902482855b5053c420a209d201420401
T-rav <tmfrisinger@gmail.com>
2017-08-04 21:09:35 +0200

    Init commit

---
+++ source/CSharp-Kata-Seed/TestClass.cs
@@ -0,0 +1,20 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+
+namespace CSharp_Kata_Seed
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void Method_WhenTestSenario_ShouldExpectedResult()
+        {
+            //---------------Set up test pack-------------------
+            
+            //---------------Execute Test ----------------------
+
+            //---------------Test Result -----------------------
+            Assert.Fail("Test Not Yet Implemented");
+        }
+    }
+}
